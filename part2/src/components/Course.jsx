1ff4586786b35d0eefcec69524ae7e10c016161b
Mrchiwizi <godpoweramaefule@gmail.com>
2026-08-23 09:49:32 -0700

    Course information step 8 with reduce method

--- part2/src/components/Course.jsx
+++ part2/src/components/Course.jsx
@@ -3,8 +3,13 @@ import Header from "./Header";
 import Content from "./Content";
 
 const Course = ({ course }) => {
-  let total = 0;
-  course.parts.forEach((content) => (total += content.exercises));
+  //   let total = 0;
+  //   course.parts.forEach((content) => (total += content.exercises));
+
+  const total = course.parts.reduce((acc, num) => acc + num.exercises, 0);
+
+  console.log("total", total);
+
   return (
     <div>
       <Header header={course.name} />
