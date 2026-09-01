6bab7a8635430c7e39420b30eaa74200e86b7be1
Mrchiwizi <godpoweramaefule@gmail.com>
2026-09-01 07:43:37 -0700

    The Phonebook Step 6

--- the-phone-book/src/components/AddContact.jsx
+++ the-phone-book/src/components/AddContact.jsx
@@ -1,31 +1,31 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
 import Search from "./Search";
 import Form from "./Form";
 import Numbers from "./Numbers";
 
 const AddContact = () => {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-1234567" },
-    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
-    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
-  ]);
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState({ name: "", number: "" });
   // const [newNumber, setNewNumber] = useState("");
 
   const [search, setSearch] = useState("");
   // console.log(search);
 
+  useEffect(() => {
+    axios.get("http://localhost:3001/persons").then((response) => {
+      console.log(response.data);
+      const allPersons = response.data;
+      setPersons(response.data);
+    });
+  }, []);
+
   const handleChange = (event) => {
     // setNewName(event.target.value);
     const { name, value } = event.target;
     setNewName({ ...newName, [name]: value });
   };
-
-  // const handleNumber = (event) => {
-  //   // const {name, value} = event.target;
-  //   setNewNumber(event.target.value);
-  //   // console.log(newNumber);
-  // };
 
   const handleFormSubmit = (event) => {
     event.preventDefault();
@@ -67,51 +67,15 @@ const AddContact = () => {
   return (
     <div>
       <h2>Phonebook</h2>
-      {/* <div>
-        <span>filter shown with:</span>{" "}
-        <input
-          type="text"
-          onChange={(e) => setSearch(e.target.value)}
-          value={search}
-        />
-      </div> */}
+
       <Search search={search} setSearch={setSearch} />
       <br />
-      {/* <form onSubmit={handleFormSubmit}>
-        <div>
-          <span> name: </span>
-          <input
-            type="text"
-            onChange={handleChange}
-            value={newName.name}
-            name="name"
-          />
-        </div>
-        <div>
-          <span>number: </span>
-          <input
-            type="number"
-            onChange={handleChange}
-            value={newName.number}
-            name="number"
-          />
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
-      </form> */}
+
       <Form
         handleFormSubmit={handleFormSubmit}
         handleChange={handleChange}
         newName={newName}
       />
-
-      {/* <h2>Numbers</h2>
-      {personsWithSearch.map((person, index) => (
-        <div key={index}>
-          {person.name} - {person.number}
-        </div>
-      ))} */}
 
       <Numbers personsWithSearch={personsWithSearch} />
 
